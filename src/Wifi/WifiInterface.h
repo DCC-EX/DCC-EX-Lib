43eb68f4c34fef46fc495bc13d39b765114ca722
dexslab <dex35803@gmail.com>
2020-07-10 23:51:42 -0400

    Remove static

--- src/Wifi/WifiInterface.h
+++ src/Wifi/WifiInterface.h
@@ -5,7 +5,7 @@
 #include "MemStream.h"
 #include <Arduino.h>
 #include <avr/pgmspace.h>
-#ifdef defined(AVR_UNO)
+#ifdef defined(ARDUINO_AVR_UNO)
 #include <SoftwareSerial.h>
 #else
 #include <HardwareSerial.h>
@@ -15,7 +15,7 @@ class WifiInterface : public CommInterface
 {
 
 public:
-#ifdef defined(AVR_UNO)
+#ifdef defined(ARDUINO_AVR_UNO)
   WifiInterface(SoftwareSerial &wifiStream, const __FlashStringHelper *SSSid, const __FlashStringHelper *password, const __FlashStringHelper *hostname, const __FlashStringHelper *servername, int port);
 #else
   WifiInterface(HardwareSerial &wifiStream, const __FlashStringHelper *SSSid, const __FlashStringHelper *password, const __FlashStringHelper *hostname, const __FlashStringHelper *servername, int port);
@@ -28,17 +28,19 @@ public:
   Stream *getStream() { return &wifiStream; }
 
 private:
-  static bool setup2(const __FlashStringHelper *SSSid, const __FlashStringHelper *password, const __FlashStringHelper *hostname, const __FlashStringHelper *servername, int port);
-  static bool checkForOK(const unsigned int timeout, const char *waitfor, bool echo);
-  static bool isHTML();
-  static bool connected;
-  static byte loopstate;
-  static int datalength;
-  static int connectionId;
-  static const byte MAX_WIFI_BUFFER = 250;
-  static char buffer[MAX_WIFI_BUFFER];
-  static MemStream streamer;
-#ifdef defined(AVR_UNO)
+  bool setup2(const __FlashStringHelper *SSSid, const __FlashStringHelper *password, const __FlashStringHelper *hostname, const __FlashStringHelper *servername, int port);
+  bool checkForOK(const unsigned int timeout, const char *waitfor, bool echo);
+  bool isHTML();
+  bool connected;
+  byte loopstate;
+  int datalength;
+  int connectionId;
+  const byte MAX_WIFI_BUFFER = 250;
+  char buffer[MAX_WIFI_BUFFER];
+  MemStream streamer;
+
+protected:
+#ifdef defined(ARDUINO_AVR_UNO)
   SoftwareSerial &wifiStream;
 #else
   HardwareSerial &wifiStream;
