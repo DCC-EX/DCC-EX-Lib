84712e3b296a9ff5e9f1e9497355fbf7a93afd86
dexslab <dex35803@gmail.com>
2020-07-11 21:53:34 -0400

    streamer removal temp

--- src/Wifi/WifiInterface.cpp
+++ src/Wifi/WifiInterface.cpp
@@ -41,7 +41,7 @@ WifiInterface::WifiInterface(HardwareSerial &wifiSerial, const __FlashStringHelp
   DIAG(F("\n++++++ Wifi Setup In Progress Using HW Serial ++++++++\n"));
   wifiStream.begin(115200);
   wifiStream.flush();
-  MemStream streamer(buffer, sizeof(buffer));
+ //MemStream streamer(buffer, sizeof(buffer));
   connected = setup2(SSid, password, hostname, servername, port);
 
   // TODO calloc the buffer and streamer and parser etc
