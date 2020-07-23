9a3a2fac70c3bba5eb30750f09aa4a1256ea90ad
dexslab <dex35803@gmail.com>
2020-07-23 19:30:36 -0400

    ESP Test 1

---
+++ src/CommInterface/CommInterfaceESP.cpp
@@ -0,0 +1,80 @@
+/*
+ *  CommInterfaceSerial.cpp
+ * 
+ *  This file is part of CommandStation.
+ *
+ *  CommandStation is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CommandStation is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CommandStation.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "CommInterfaceSerial.h"
+
+#include <Arduino.h>
+
+#include "CommManager.h"
+#include "DCCEXParser.h"
+
+#if defined(ARDUINO_AVR_UNO)
+SerialInterface::SerialInterface(SoftwareSerial &serial, long baud) : serialStream(serial), baud(baud), buffer(""), inCommandPayload(false)
+{
+  serialStream.begin(baud);
+  serialStream.flush();
+}
+#else
+SerialInterface::SerialInterface(HardwareSerial &serial, long baud) : serialStream(serial), baud(baud), buffer(""), inCommandPayload(false)
+{
+  serialStream.begin(baud);
+  serialStream.flush();
+}
+#endif
+
+void SerialInterface::process()
+{
+  while (serialStream.available())
+  {
+    char ch = serialStream.read();
+    if (ch == '[')
+    {
+      inCommandPayload = true;
+      buffer = "";
+    }
+    else if (ch == ']')
+    {
+      int id = buffer.charAt(0);
+      buffer.remove(0, 3);
+      DCCEXParser::parse(buffer.c_str());
+      buffer = "";
+      inCommandPayload = false;
+    }
+    else if (inCommandPayload)
+    {
+      buffer += ch;
+    }
+  }
+}
+
+void SerialInterface::showConfiguration()
+{
+  serialStream.print("[0 Wifi Serial - Speed:]");
+  serialStream.println(baud);
+}
+
+void SerialInterface::showInitInfo()
+{
+  CommManager::printf("[0 <N0:SERIAL>]");
+}
+
+void SerialInterface::send(const char *buf)
+{
+  CommManager::printf(F("[0 %s]"), buf);
+}
---
+++ src/CommInterface/CommInterfaceESP.h
@@ -0,0 +1,57 @@
+/*
+ *  CommInterfaceSerial.h
+ * 
+ *  This file is part of CommandStation.
+ *
+ *  CommandStation is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CommandStation is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CommandStation.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef COMMANDSTATION_COMMINTERFACE_COMMINTERFACESERIAL_H_
+#define COMMANDSTATION_COMMINTERFACE_COMMINTERFACESERIAL_H_
+
+#include "CommInterface.h"
+
+#include <Arduino.h>
+#if defined(ARDUINO_AVR_UNO)
+#include <SoftwareSerial.h>
+#else
+#include <HardwareSerial.h>
+#endif
+
+class SerialInterface : public CommInterface
+{
+public:
+#if defined(ARDUINO_AVR_UNO)
+	SerialInterface(SoftwareSerial &serial, long baud = 9600);
+#else
+	SerialInterface(HardwareSerial &serial, long baud = 115200);
+#endif
+	void process();
+	void showConfiguration();
+	void showInitInfo();
+	void send(const char *buf);
+	Stream *getStream() { return &serialStream; }
+
+protected:
+#if defined(ARDUINO_AVR_UNO)
+	SoftwareSerial &serialStream;
+#else
+	HardwareSerial &serialStream;
+#endif
+	long baud;
+	String buffer;
+	bool inCommandPayload;
+};
+
+#endif // COMMANDSTATION_COMMINTERFACE_COMMINTERFACESERIAL_H_
